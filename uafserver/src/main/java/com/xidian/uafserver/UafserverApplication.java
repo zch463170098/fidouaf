45d2a5fa03ad9fe0b8209788ff93632d369fdfc7
DESKTOP-VI1V0PI\zch <463170098@qq.com>
2018-08-26 11:00:24 +0800

    [zch] # 搭建uaf server 基础框架

---
+++ uafserver/src/main/java/com/xidian/uafserver/UafserverApplication.java
@@ -0,0 +1,14 @@
+package com.xidian.uafserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class UafserverApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(UafserverApplication.class, args);
+    }
+}
---
+++ uafserver/src/test/java/com/xidian/uafserver/UafserverApplicationTests.java
@@ -0,0 +1,16 @@
+package com.xidian.uafserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UafserverApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
