bada2d914949ead292b3853068b16f1e7e844cc9
Zhang Chenghui <463170098@qq.com>
2018-08-27 09:32:50 +0800

    添加userDao

---
+++ uafserver/src/main/java/com/xidian/uafserver/Dao/UserDao.java
@@ -0,0 +1,10 @@
+package com.xidian.uafserver.Dao;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@Mapper
+public interface UserDao {
+
+}
